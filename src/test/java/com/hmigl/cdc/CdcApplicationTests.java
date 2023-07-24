3f9803587b3d3d7172aab8a7e1a80b90ae460645
hmigl <88746406+hmigl@users.noreply.github.com>
2023-07-24 19:46:02 -0300

    Initial commit

---
+++ src/test/java/com/hmigl/cdc/CdcApplicationTests.java
@@ -0,0 +1,11 @@
+package com.hmigl.cdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
