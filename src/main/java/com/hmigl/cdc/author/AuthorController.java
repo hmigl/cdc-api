d33a20782df49f50c8ec785400114fa72c53c903
hmigl <88746406+hmigl@users.noreply.github.com>
2023-07-24 21:54:14 -0300

    feat: persist new author

--- src/main/java/com/hmigl/cdc/author/AuthorController.java
+++ src/main/java/com/hmigl/cdc/author/AuthorController.java
@@ -1,6 +1,10 @@
 package com.hmigl.cdc.author;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/authors")
 public class AuthorController {
 
+    private final @PersistenceContext EntityManager entityManager;
+
+    public AuthorController(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @PostMapping
+    @Transactional
     public String register(@Valid @RequestBody AuthorDTO authorDTO) {
-        return authorDTO.toString();
+        var author = Author.fromDTO(authorDTO);
+        entityManager.persist(author);
+        return author.toString();
     }
 }
