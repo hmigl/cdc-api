82c25804ebe73c129e1169e9c4be104db9fd1c31
hmigl <88746406+hmigl@users.noreply.github.com>
2023-07-24 21:46:44 -0300

    refactor: remove 'instant' property

--- src/main/java/com/hmigl/cdc/author/AuthorDTO.java
+++ src/main/java/com/hmigl/cdc/author/AuthorDTO.java
@@ -2,25 +2,9 @@ package com.hmigl.cdc.author;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
-
-import java.time.Instant;
 
 public record AuthorDTO(
         @NotBlank String name,
         @NotBlank @Email String email,
-        @NotBlank @Size(max = 400) String description,
-        @PastOrPresent Instant instant) {
-
-    public AuthorDTO(
-            @NotBlank String name,
-            @NotBlank @Email String email,
-            @NotBlank @Size(max = 400) String description,
-            @PastOrPresent Instant instant) {
-        this.name = name;
-        this.email = email;
-        this.description = description;
-        this.instant = Instant.now();
-    }
-}
+        @NotBlank @Size(max = 400) String description) {}
