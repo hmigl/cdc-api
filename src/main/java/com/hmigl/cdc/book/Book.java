8405f3ded4b49b99f29a51c834178d88d7a0700c
hmigl <88746406+hmigl@users.noreply.github.com>
2023-07-27 18:11:43 -0300

    refactor: add category and author ids

--- src/main/java/com/hmigl/cdc/book/Book.java
+++ src/main/java/com/hmigl/cdc/book/Book.java
@@ -23,9 +23,11 @@ public class Book {
     private @NotBlank @Size(max = 500) String overview;
     private @NotBlank String summary;
     private @NotNull @Min(20) BigDecimal price;
-    private @NotNull @Min(100) Integer pages;
+    private @NotNull @Min(100) Long pages;
     private @NotBlank @ISBN(type = ISBN.Type.ANY) String isbn;
     private @NotNull @Future LocalDateTime publicationDate;
+    private @NotNull Long categoryId;
+    private @NotNull Long authorId;
 
     @Deprecated
     protected Book() {}
@@ -35,9 +37,11 @@ public class Book {
             @NotBlank @Size(max = 500) String overview,
             @NotBlank String summary,
             @NotNull @Min(20) BigDecimal price,
-            @NotNull @Min(100) Integer pages,
+            @NotNull @Min(100) Long pages,
             @NotBlank @ISBN(type = ISBN.Type.ANY) String isbn,
-            @NotNull @Future LocalDateTime publicationDate) {
+            @NotNull @Future LocalDateTime publicationDate,
+            @NotNull Long categoryId,
+            @NotNull Long authorId) {
         this.title = title;
         this.overview = overview;
         this.summary = summary;
@@ -45,6 +49,8 @@ public class Book {
         this.pages = pages;
         this.isbn = isbn;
         this.publicationDate = publicationDate;
+        this.categoryId = categoryId;
+        this.authorId = authorId;
     }
 
     public static Book fromDTO(@NotNull BookDTO bookDTO) {
@@ -71,6 +77,10 @@ public class Book {
                 bookDTO.publicationDate(),
                 "a publication date must be provided"); // TODO: @Future validation?
 
+        Assert.notNull(bookDTO.categoryId(), "a category must be provided");
+
+        Assert.notNull(bookDTO.authorId(), "an author must be specified");
+
         return new Book(
                 bookDTO.title(),
                 bookDTO.overview(),
@@ -78,7 +88,9 @@ public class Book {
                 bookDTO.price(),
                 bookDTO.pages(),
                 bookDTO.isbn(),
-                bookDTO.publicationDate());
+                bookDTO.publicationDate(),
+                bookDTO.categoryId(),
+                bookDTO.authorId());
     }
 
     public Long getId() {
--- src/main/java/com/hmigl/cdc/book/BookDTO.java
+++ src/main/java/com/hmigl/cdc/book/BookDTO.java
@@ -1,24 +1,24 @@
 package com.hmigl.cdc.book;
 
+import com.hmigl.cdc.author.Author;
+import com.hmigl.cdc.category.Category;
+import com.hmigl.cdc.shared.IdExists;
 import com.hmigl.cdc.shared.UniqueValue;
 
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-// TODO: add category and author
 public record BookDTO(
         @NotBlank @UniqueValue(domainClass = Book.class, fieldName = "title") String title,
         @NotBlank @Size(max = 500) String overview,
         @NotBlank String summary,
         @NotNull @Min(20) BigDecimal price,
-        @NotNull @Min(100) Integer pages,
+        @NotNull @Min(100) Long pages,
         @NotBlank @ISBN(type = ISBN.Type.ANY) @UniqueValue(domainClass = Book.class, fieldName = "isbn") String isbn,
-        @NotNull @Future LocalDateTime publicationDate) {}
+        @NotNull @Future LocalDateTime publicationDate,
+        @NotNull @IdExists(domainClass = Category.class, fieldName = "id") Long categoryId,
+        @NotNull @IdExists(domainClass = Author.class, fieldName = "id") Long authorId) {}
