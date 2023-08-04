b0c012914dbb23ede06cb62c9a630404cb30d332
hmigl <88746406+hmigl@users.noreply.github.com>
2023-08-04 09:01:58 -0300

    feat: persist purchase

--- src/main/java/com/hmigl/cdc/payment_flow/PurchaseAttemptController.java
+++ src/main/java/com/hmigl/cdc/payment_flow/PurchaseAttemptController.java
@@ -2,6 +2,7 @@ package com.hmigl.cdc.payment_flow;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
@@ -11,10 +12,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 // 5
 @RestController
-@RequestMapping("/api/v1/payments")
+@RequestMapping("/api/v1/purchases")
 public class PurchaseAttemptController {
     private @PersistenceContext EntityManager manager;
 
@@ -38,9 +40,15 @@ public class PurchaseAttemptController {
     }
 
     @PostMapping
-    public ResponseEntity<?> process(
+    @Transactional
+    public ResponseEntity<PurchaseAttemptRequest> process(
             @Valid @RequestBody PurchaseAttemptRequest purchaseAttemptRequest) {
-        Purchase newPurchase = purchaseAttemptRequest.toModel(manager);
-        return ResponseEntity.ok(newPurchase.toString());
+        var purchase = purchaseAttemptRequest.toModel(manager);
+        manager.persist(purchase);
+        var uri =
+                UriComponentsBuilder.fromPath("/purchases/{id}")
+                        .buildAndExpand(purchase.getId())
+                        .toUri();
+        return ResponseEntity.created(uri).body(purchaseAttemptRequest);
     }
 }
